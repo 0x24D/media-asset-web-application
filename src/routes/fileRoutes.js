91ad61e6ab66bdb36e98ab0dfb5f35e778379324
0x24d <daniel.longfield@tuta.io>
2019-01-18 17:35:30 +0000

    Add middleware todo comments

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -5,6 +5,7 @@ import {
 } from '../controllers/fileController';
 
 const fileRoutes = (app) => {
+    // TODO: add middleware for checking user authentication on all routes
     app.route('/v1/files')
     .get(getFiles)
     .post(addNewFile)
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,7 @@ import {
 } from '../controllers/userController';
 
 const userRoutes = (app) => {
+    // TODO: add middleware for checking user authentication on all routes
     app.route('/v1/users')
     .get((req, res, next) => {
         // middleware
