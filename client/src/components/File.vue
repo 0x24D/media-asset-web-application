7a0ef2e94b772052dfefdcb5b071e78f8600fe53
0x24d <daniel.longfield@tuta.io>
2019-02-19 14:11:52 +0000

    Implement viewing multiple versions of a file

--- client/src/components/File.vue
+++ client/src/components/File.vue
@@ -1,7 +1,67 @@
 <template>
   <div id="file">
-    TODO: implement view<br/>
-    {{ file.title }}
+    <dialog open>
+      <button id="leftButton" @click="viewEarlierVersion(currentVersion.version)"
+        :disabled="currentVersion.version === 1">Left</button>
+        <div id="container">
+          <table>
+            <tr>
+              <td>
+                Name:
+              </td>
+              <td>
+                {{ fileVersions[latestVersion - currentVersion.version].name  }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Title:
+              </td>
+              <td>
+                {{ fileVersions[latestVersion - currentVersion.version].title }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Author:
+              </td>
+              <td>
+                {{ fileVersions[latestVersion - currentVersion.version].author }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Tags:
+              </td>
+              <td>
+                <div v-for="tag in fileVersions[latestVersion - currentVersion.version].tags"
+                  :key="tag">
+                  {{ tag }}
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Created date:
+              </td>
+              <td>
+                {{ fileVersions[latestVersion - currentVersion.version].created_date }}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Version:
+              </td>
+              <td>
+                {{ fileVersions[latestVersion - currentVersion.version].version }}
+              </td>
+            </tr>
+          </table>
+        </div>
+        <button id="rightButton" @click="viewLaterVersion(currentVersion.version)"
+          :disabled="currentVersion.version === latestVersion">Right</button>
+        <button id="closeButton" @click="viewFileClose()">Close</button>
+    </dialog>
   </div>
 </template>
 
@@ -13,14 +73,18 @@ export default {
   },
   data() {
     return {
-      file: {},
+      latestVersion: '',
+      currentVersion: {},
+      fileVersions: [],
     };
   },
   created() {
     this.$axios
       .get(`http://localhost:8081/api/v1/files/${this.fileId}`)
       .then((response) => {
-        this.file = response.data;
+        this.currentVersion = response.data;
+        this.latestVersion = this.currentVersion.version;
+        this.fileVersions.push(this.currentVersion);
       })
       .catch((error) => {
         if (error.response) {
@@ -40,6 +104,67 @@ export default {
         }
         console.log(error.config);
       });
+  },
+  methods: {
+    viewFileClose() {
+      this.$store.commit('setFileDisplayMode', false);
+    },
+    viewEarlierVersion(currentVersion) {
+      this.$axios
+        .get(`http://localhost:8081/api/v1/files/${this.fileId}/${currentVersion - 1}`)
+        .then((response) => {
+          this.currentVersion = response.data;
+          if (!this.fileVersions.some(e => e.version === this.currentVersion.version)) {
+            this.fileVersions.push(this.currentVersion);
+          }
+        })
+        .catch((error) => {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+        });
+    },
+    viewLaterVersion(currentVersion) {
+      this.$axios
+        .get(`http://localhost:8081/api/v1/files/${this.fileId}/${currentVersion + 1}`)
+        .then((response) => {
+          this.currentVersion = response.data;
+          if (!this.fileVersions.some(e => e.version === this.currentVersion.version)) {
+            this.fileVersions.push(this.currentVersion);
+          }
+        })
+        .catch((error) => {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+          console.log(error.config);
+        });
+    },
   },
 };
 </script>
