bdc08ef4e85ec40b85759a505df397cf43d1862a
0x24d <daniel.longfield@tuta.io>
2019-02-20 19:46:04 +0000

    Return HTTP 409 if the user already has a token

--- server/src/controllers/authenticationController.js
+++ server/src/controllers/authenticationController.js
@@ -18,44 +18,48 @@ export const loginUser = (req, res) => {
         },
       );
     } else if (user) {
-      bcrypt.hash(unHashedPassword, user.salt, (err2, hash) => {
-        if (err2) {
-          res.status(500).send(
-            {
-              err: 'An error has occured logging in, please try again later',
-              extended: err2,
-            },
-          );
-        } else {
-          hashedPassword = hash;
-          if (user.password === hashedPassword) {
-            const encoder = new Paseto.V2();
-            encoder.symmetric()
-              .then((sk) => {
-                const message = `${user.username}+${Date.now()}`;
-                return encoder.encrypt(message, sk);
-              })
-              .then((token) => {
-                const newUser = user;
-                newUser.token = token;
-                updateExistingByUsername(user.username, newUser, (err3) => {
-                  if (err3) {
-                    res.status(500).send(
-                      {
-                        err: 'An error has occured logging in, please try again later',
-                        extended: err3,
-                      },
-                    );
-                  } else {
-                    res.json({ token });
-                  }
-                });
-              });
+      if (user.token) {
+        res.status(409).end();
+      } else {
+        bcrypt.hash(unHashedPassword, user.salt, (err2, hash) => {
+          if (err2) {
+            res.status(500).send(
+              {
+                err: 'An error has occured logging in, please try again later',
+                extended: err2,
+              },
+            );
           } else {
-            res.status(500).send({ err: 'Username or password is not correct' });
+            hashedPassword = hash;
+            if (user.password === hashedPassword) {
+              const encoder = new Paseto.V2();
+              encoder.symmetric()
+                .then((sk) => {
+                  const message = `${user.username}+${Date.now()}`;
+                  return encoder.encrypt(message, sk);
+                })
+                .then((token) => {
+                  const newUser = user;
+                  newUser.token = token;
+                  updateExistingByUsername(user.username, newUser, (err3) => {
+                    if (err3) {
+                      res.status(500).send(
+                        {
+                          err: 'An error has occured logging in, please try again later',
+                          extended: err3,
+                        },
+                      );
+                    } else {
+                      res.json({ token });
+                    }
+                  });
+                });
+            } else {
+              res.status(500).send({ err: 'Username or password is not correct' });
+            }
           }
-        }
-      });
+        });
+      }
     } else {
       res.status(500).send({ err: 'That username does not exist' });
     }
