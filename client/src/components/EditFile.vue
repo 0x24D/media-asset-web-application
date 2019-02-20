da59e6f24cdd50e2cbae03cb6b0c02770624ec20
0x24d <daniel.longfield@tuta.io>
2019-02-20 15:12:09 +0000

    Add basic form validation

--- client/src/components/EditFile.vue
+++ client/src/components/EditFile.vue
@@ -5,11 +5,12 @@
         <label for="name">File name: </label>
         <input type="text" id="name" v-model="file.name" disabled><br/>
         <label for="title">Title: </label>
-        <input type="text" id="title" v-model="file.title"><br/>
+        <input type="text" id="title" v-model="file.title" required><br/>
         <label for="author">Author: </label>
-        <input type="text" id="author" v-model="file.author"><br/>
+        <input type="text" id="author" v-model="file.author" required><br/>
         <label for="tags">Tags: </label>
-        <input type="text" id="tags" v-model="file.tags"><br/>
+        <input type="text" id="tags" v-model="file.tags"
+          title="Comma-separated list of tags" pattern="(\w+)(,\s*\w+)*"><br/>
         <br/>
 
         <input type="submit" value="Submit" @click="editFileSubmit(file._id, file)">
@@ -57,38 +58,45 @@ export default {
       });
   },
   methods: {
+    checkFormValidity() {
+      return document.getElementById('tags').checkValidity()
+      && document.getElementById('author').checkValidity()
+      && document.getElementById('tags').checkValidity();
+    },
     editFileClose() {
       this.$store.commit('setEditFileDisplayMode', false);
     },
     editFileSubmit(fileId, formData) {
-      this.$axios
-        .put(`http://localhost:8081/api/v1/files/${fileId}`, {
-          title: formData.title,
-          author: formData.author,
-          tags: Array.isArray(formData.tags) ? formData.tags : formData.tags.split(','),
-        })
-        .then(() => {
-          this.$store.commit('setEditFileDisplayMode', false);
-          window.location.reload();
-        })
-        .catch((error) => {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-          console.log(error.config);
-        });
+      if (this.checkFormValidity()) {
+        this.$axios
+          .put(`http://localhost:8081/api/v1/files/${fileId}`, {
+            title: formData.title,
+            author: formData.author,
+            tags: Array.isArray(formData.tags) ? formData.tags : formData.tags.split(','),
+          })
+          .then(() => {
+            this.$store.commit('setEditFileDisplayMode', false);
+            window.location.reload();
+          })
+          .catch((error) => {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
+          });
+      }
     },
   },
 };
--- client/src/components/NewFile.vue
+++ client/src/components/NewFile.vue
@@ -1,15 +1,16 @@
 <template>
-  <div id="editFile">
+  <div id="newFile">
     <dialog open>
       <form @submit.prevent>
         <label for="name">File name: </label>
-        <input type="text" id="name" v-model="file.name"><br/>
+        <input type="text" id="name" v-model="file.name" required><br/>
         <label for="title">Title: </label>
-        <input type="text" id="title" v-model="file.title"><br/>
+        <input type="text" id="title" v-model="file.title" required><br/>
         <label for="author">Author: </label>
-        <input type="text" id="author" v-model="file.author"><br/>
+        <input type="text" id="author" v-model="file.author" required><br/>
         <label for="tags">Tags: </label>
-        <input type="text" id="tags" v-model="file.tags"><br/>
+        <input type="text" id="tags" v-model="file.tags"
+          title="Comma-separated list of tags" pattern="(\w+)(,\s*\w+)*"><br/>
         <br/>
 
         <input type="submit" value="Submit" @click="newFileSubmit(file._id, file)">
@@ -33,39 +34,47 @@ export default {
     };
   },
   methods: {
+    checkFormValidity() {
+      return document.getElementById('name').checkValidity()
+      && document.getElementById('tags').checkValidity()
+      && document.getElementById('author').checkValidity()
+      && document.getElementById('tags').checkValidity();
+    },
     newFileClose() {
       this.$store.commit('setNewFileDisplayMode', false);
     },
     newFileSubmit(fileId, formData) {
-      this.$axios
-        .post('http://localhost:8081/api/v1/files/', {
-          name: formData.name,
-          title: formData.title,
-          author: formData.author,
-          tags: formData.tags.split(','),
-        })
-        .then(() => {
-          this.$store.commit('setNewFileDisplayMode', false);
-          window.location.reload();
-        })
-        .catch((error) => {
-          if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-          } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-          }
-          console.log(error.config);
-        });
+      if (this.checkFormValidity()) {
+        this.$axios
+          .post('http://localhost:8081/api/v1/files/', {
+            name: formData.name,
+            title: formData.title,
+            author: formData.author,
+            tags: formData.tags.split(','),
+          })
+          .then(() => {
+            this.$store.commit('setNewFileDisplayMode', false);
+            window.location.reload();
+          })
+          .catch((error) => {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
+          });
+      }
     },
   },
 };
